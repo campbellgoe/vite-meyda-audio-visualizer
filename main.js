877127e456a6bc6f312448d89501b2caa764f32f
George Campbell <campbell.goe@gmail.com>
2023-12-11 03:29:44 +0000

    wave is cooler now

--- main.js
+++ main.js
@@ -5,11 +5,15 @@ const fileInput = document.getElementById('audioFile');
 const audioPlayer = document.getElementById('audioPlayer');
 const canvas = document.getElementById('visualizer');
 const ctx = canvas.getContext('2d');
+const modeBarBtn = document.getElementById('mode-bar');
+const modeWaveBtn = document.getElementById('mode-wave');
+
 canvas.width = window.innerWidth * 0.8;
 canvas.height = 300;
 
 let micRecorder;
 let recordingMic = false;
+let visualizationMode = 'wave'; // 'bar' or 'wave'
 
 function updateRecordMicBtnText(recording) {
   recordMicBtn.textContent = recording ? 'Stop Microphone' : 'Record Microphone';
@@ -26,8 +30,8 @@ async function startMicAndDrawAudio() {
     audioContext: audioCtx,
     source: audioSource,
     bufferSize: 512,
-    featureExtractors: ['rms'],
-    callback: features => drawAudio({ rms: features.rms })
+    featureExtractors: ['rms', 'amplitudeSpectrum'],
+    callback: drawAudio
   });
 
   analyzer.start();
@@ -63,24 +67,67 @@ audioPlayer.onplay = () => {
     audioContext: audioCtx,
     source: source,
     bufferSize: 512,
-    featureExtractors: ['rms'],
+    featureExtractors: ['rms', 'amplitudeSpectrum'],
     callback: drawAudio
   });
 
   analyzer.start();
 };
 
-function drawAudio({ rms }) {
+
+function drawWave({rms, amplitudeSpectrum}) {
+  console.log(rms, amplitudeSpectrum)
+  
   ctx.fillStyle = 'rgba(0,0,0,0.1)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  const height = rms * 200; // Scale the height of the line with RMS value
+  const amplitude = rms * canvas.height/2; // Scaling factor for wave amplitude
+  ctx.beginPath();
+
+  ctx.moveTo(0, canvas.height / 2);
+  for (let i = 0; i < canvas.width; i++) {
+    const j = i/canvas.width
+    const k = Math.floor(j**2*256**0.5)
+    const ampSpectral = amplitudeSpectrum[k]
+    // Generating a sine wave pattern
+    const angle = (i / canvas.width) * 2 * Math.PI; // Normalizing the angle
+    const sinValue = Math.sin(angle * 10); // Frequency of the wave
+    const y = canvas.height / 2 + sinValue * amplitude *(1+ampSpectral) * (Math.sin(j*Math.PI));
+    ctx.lineTo(i, y);
+  }
+
+  ctx.strokeStyle = '#558822';
+  ctx.lineWidth = 2; // Width of the wave line
+  ctx.stroke();
+}
+
+function drawBar(rms) {
+  ctx.fillStyle = 'rgba(0,0,0,0.1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const height = rms * canvas.height/2;
   const y = canvas.height / 2 - height / 2;
 
   ctx.fillStyle = '#558822';
   ctx.fillRect(0, y, canvas.width, height);
 }
 
-window.onresize = () => {
+function drawAudio({ rms, amplitudeSpectrum }) {
+  if (visualizationMode === 'bar') {
+  drawBar(rms);
+  } else if (visualizationMode === 'wave') {
+  drawWave({rms, amplitudeSpectrum});
+  }
+  }
+  
+  modeBarBtn.addEventListener('click', () => {
+  visualizationMode = 'bar';
+  });
+  
+  modeWaveBtn.addEventListener('click', () => {
+  visualizationMode = 'wave';
+  });
+  
+  window.onresize = () => {
   canvas.width = window.innerWidth * 0.8;
-};
+  };
