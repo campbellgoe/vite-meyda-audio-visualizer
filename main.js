d4ef0bb00d10271cfaa6eb6d4e143d97b7c50baa
George Campbell <campbell.goe@gmail.com>
2023-12-11 02:30:54 +0000

    improvement

--- main.js
+++ main.js
@@ -1,58 +1,54 @@
 import Meyda from 'meyda';
 
-const recordMicBtn = document.getElementById('record-mic-btn')
+const recordMicBtn = document.getElementById('record-mic-btn');
 const fileInput = document.getElementById('audioFile');
 const audioPlayer = document.getElementById('audioPlayer');
 const canvas = document.getElementById('visualizer');
 const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth * 0.8;
+canvas.height = 300;
 
+let micRecorder;
+let recordingMic = false;
 
-let micRecorder
-let recordingMic = false
-function updateRecordMicBtnText(recordingMic) {
-  if (recordingMic) {
-    recordMicBtn.textContent = 'Stop microphone'
-  } else {
-    recordMicBtn.textContent = 'Record microphone'
-  }
+function updateRecordMicBtnText(recording) {
+  recordMicBtn.textContent = recording ? 'Stop Microphone' : 'Record Microphone';
 }
+
 async function startMicAndDrawAudio() {
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
-  recordingMic = !!stream
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  recordingMic = !!stream;
   micRecorder = new MediaRecorder(stream);
-  // console.log('You let me use your mic!', stream)
   const audioCtx = new AudioContext();
   const audioSource = audioCtx.createMediaStreamSource(stream);
-  // // console.log('audioSource', audioSource)
-  // WARN: DO NOT connect source (mic) to audio destination (speakers) because it can lead to feedback
-  const analyzer = Meyda.createMeydaAnalyzer({
-    "audioContext": audioCtx,
-    "source": audioSource,
-    "bufferSize": 512,
-    "featureExtractors": ['rms'],
-    "callback": features => drawMicAudioData(features)
 
+  const analyzer = Meyda.createMeydaAnalyzer({
+    audioContext: audioCtx,
+    source: audioSource,
+    bufferSize: 512,
+    featureExtractors: ['rms'],
+    callback: features => drawAudio({ rms: features.rms })
   });
+
   analyzer.start();
 }
-function endMic() {
-  micRecorder.stream.getAudioTracks().forEach(function (track) { track.stop(); });
-  recordingMic = false
-}
-recordMicBtn.addEventListener('click', function () {
-  async function toggle() {
-    if (!recordingMic) {
-      await startMicAndDrawAudio()
-      updateRecordMicBtnText(true)
-    } else {
-      endMic() 
-      updateRecordMicBtnText(false)
-    }
-  }
-  toggle()
-})
 
-fileInput.addEventListener('change', function (e) {
+function endMic() {
+  micRecorder.stream.getAudioTracks().forEach(track => track.stop());
+  recordingMic = false;
+}
+
+recordMicBtn.addEventListener('click', async () => {
+  if (!recordingMic) {
+    await startMicAndDrawAudio();
+    updateRecordMicBtnText(true);
+  } else {
+    endMic();
+    updateRecordMicBtnText(false);
+  }
+});
+
+fileInput.addEventListener('change', e => {
   const file = e.target.files[0];
   const url = URL.createObjectURL(file);
   audioPlayer.src = url;
@@ -61,9 +57,8 @@ fileInput.addEventListener('change', function (e) {
 audioPlayer.onplay = () => {
   const audioCtx = new AudioContext();
   const source = audioCtx.createMediaElementSource(audioPlayer);
+  source.connect(audioCtx.destination);
 
-  // connect audio source to speakers
-  source.connect(audioCtx.destination)
   const analyzer = Meyda.createMeydaAnalyzer({
     audioContext: audioCtx,
     source: source,
@@ -71,25 +66,21 @@ audioPlayer.onplay = () => {
     featureExtractors: ['rms'],
     callback: drawAudio
   });
+
   analyzer.start();
 };
 
 function drawAudio({ rms }) {
-  const w = canvas.width
-  const h = canvas.height
-  ctx.fillStyle = 'rgba(0,0,0,0.1)'
+  ctx.fillStyle = 'rgba(0,0,0,0.1)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = '#558822'
-  ctx.beginPath()
-  ctx.moveTo(0, h / 2)
-  ctx.lineTo(w, h / 2);
-  ctx.lineWidth = rms;
-  ctx.closePath();
-  ctx.stroke();
-  // Visualization logic here, using 'rms' or other features
+
+  const height = rms * 200; // Scale the height of the line with RMS value
+  const y = canvas.height / 2 - height / 2;
+
+  ctx.fillStyle = '#558822';
+  ctx.fillRect(0, y, canvas.width, height);
 }
 
-function drawMicAudioData(features) {
-  //console.log(features)
-  drawAudio({ rms: features.rms })
-}
+window.onresize = () => {
+  canvas.width = window.innerWidth * 0.8;
+};
